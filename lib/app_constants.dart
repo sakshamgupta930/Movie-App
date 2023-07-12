e16fb7180188b22a2b1f29d4fa82758a608421d4
Saksham gupta <guptasaksham3071gmail.com>
2023-07-12 09:47:00 +0530

    initial and setup project setup splash screen

---
+++ lib/app_constants.dart
@@ -0,0 +1,7 @@
+class AppConstants {
+  static const String baseUrl = 'https://api.themoviedb.org/3';
+  static const String imageUrlW500 = 'https://image.tmdb.org/t/p/w500';
+  static const String imageUrlOriginal = 'https://image.tmdb.org/t/p/original';
+  static const String apiKey = '38a3ea8af7405f287b708ef20834349f';
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  runApp(const MyApp());
+  FlutterNativeSplash.remove();
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
